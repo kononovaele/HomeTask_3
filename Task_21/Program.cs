02348867c7ccd5a1452ce1f1bc8cfcd19ca39068
Abramov Sergey <Kononovaele@yandex.ru>
2023-01-13 18:49:15 +0300

    add task_23

---
+++ Task_21/Program.cs
@@ -0,0 +1,36 @@
+﻿//
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
+//  между ними в 3D пространстве
+//
+using System;
+
+// Функция принимает три аргумента (целые числа) и возвращает расстояние между ними (вещественное число)
+double GetDistance(int x, int y, int z)
+{
+
+    return Math.Sqrt(x * x + y * y + z * z);
+
+}
+
+void main()
+{
+    Console.WriteLine(" ------- Task-21 -------");
+
+    int x = 1;
+    int y = 1;
+    int z = 1;
+    //Сonsole.WriteLine(GetDistance(x, y, z));
+    //Сonsole.Write(12);
+
+    x = 2; y = 2; z = 2;
+    //Сonsole.WriteLine(GetDistance(x, y, z));
+
+    x = 10;
+    y = 10;
+    z = 10;
+    //Сonsole.WriteLine(GetDistance(x, y, z));
+
+}
+
+main();
+
---
+++ Task_23/Program.cs
@@ -0,0 +1,36 @@
+﻿//
+// Задача 23. Напишите программу, которая принимает на вход числа (N) и выдает таблицу кубов от 1 до N
+//
+
+// Вывод в строку число и значения его кубов от 1 до значения числа n
+void PrintStrOfTable(int n)
+{
+    int value;
+
+    Console.WriteLine("");
+    Console.Write(n);
+    Console.Write(" -> ");
+
+    for(int i = 1; i <= n; ++i)
+    {
+        value = i * i * i;
+        Console.Write(value);
+        if(i != n)
+        {
+            Console.Write(", ");
+        }
+    }
+
+}
+
+void main()
+{
+    Console.WriteLine("\n ------- Task-23 -------");
+
+    PrintStrOfTable(3);
+    PrintStrOfTable(5);
+
+}
+
+main();
+
